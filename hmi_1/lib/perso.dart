fe03e0d9eb2ee8454d374295be626eff98a9ca24
HugeAG <h.a.g.trabajo@gmail.com>
2024-12-11 15:14:49 +0100

    Avances 1 en perso.dart
    Este código es básicamente la pestaña de compra de un disco, se llama así pq puedes personalizar el disco

--- hmi_1/lib/perso.dart
+++ hmi_1/lib/perso.dart
@@ -1,12 +1,59 @@
 import 'package:flutter/material.dart';
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Persona tu propio Disco'
+      home: VinylCustomizationScreen(),
+    );
+  }
+}
+
+class VinylCustomizationScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2, // Número de pestañas
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Personalizar Disco de Vinilo'),
+          bottom: TabBar(
+            tabs: [
+              Tab(icon: Icon(Icons.image), text: 'Cargar Imagen'),
+              Tab(icon: Icon(Icons.edit), text: 'Editar Diseño'),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            // Primera pestaña: Cargar Imagen
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Image.asset('assets/R.jpg'),
+                  ElevatedButton(
+                    onPressed: () {
+                      // Lógica para cargar una nueva imagen
+                    },
+                    child: Text('Cargar Nueva Imagen'),
+                  ),
+                ],
+              ),
+            ),
+            // Segunda pestaña: Editar Diseño
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text('Herramientas de Edición'),
+                  // Aquí puedes agregar widgets para editar el diseño del vinilo
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
